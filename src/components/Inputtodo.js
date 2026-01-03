18bdbd1cce7856ed66b01a63f1647d814f9f9a63
rajveer2450 <rajveerpatel.2450@gmail.com>
2026-01-03 19:06:02 +0530

    added POST function in client side

--- src/components/Inputtodo.js
+++ src/components/Inputtodo.js
@@ -2,27 +2,33 @@ import React, {useState} from "react";
 const InputTodo = ()=>{
     const [description, setDescription]= useState("");
     const onSubmitform= async e =>{
-        e.preventDefault()
+        e.preventDefault();
         try{
-            const body = {description}
-            const response = fetch("")
-
+                const body = {description}
+                const response = await fetch("http://localhost:5000/todos", {
+                    method:"POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify(body)
+            });
+                // console.log(await response.json());
+                window.location='/'
+                
 
 
 
         }catch(err){
             console.error(err.message)
         }
-    }
+    };
     return(
-        <>
-        <h1 className="text-center mt-5">hello</h1>
-        <form className="d-flex m-5" onSubmit={onSubmitform}>
-            <input type="text" className="form-control" value={description} onChange={e=> setDescription(e.target.value)}/>
-            <button className="btn btn-success">add</button>
-        </form>
+        <div>
+            <h1 className="text-center mt-5">hello</h1>
+            <form className="d-flex m-5" onSubmit={onSubmitform}>
+                <input type="text" className="form-control" value={description} onChange={e=> setDescription(e.target.value)}/>
+                <button className="btn btn-success">add</button>
+            </form>
 
-        </>
+        </div>
         
     )
 }
