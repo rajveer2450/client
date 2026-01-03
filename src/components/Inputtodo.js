bff71cc7cf01278b62f69d236980800af1eedb65
rajveer2450 <rajveerpatel.2450@gmail.com>
2026-01-03 18:31:34 +0530

    form added

---
+++ src/components/Inputtodo.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+const InputTodo = ()=>{
+    const [description, setDescription]= useState("");
+    const onSubmitform= async e =>{
+        e.preventDefault()
+        try{
+            const body = {description}
+            const response = fetch("")
+
+
+
+
+        }catch(err){
+            console.error(err.message)
+        }
+    }
+    return(
+        <>
+        <h1 className="text-center mt-5">hello</h1>
+        <form className="d-flex m-5" onSubmit={onSubmitform}>
+            <input type="text" className="form-control" value={description} onChange={e=> setDescription(e.target.value)}/>
+            <button className="btn btn-success">add</button>
+        </form>
+
+        </>
+        
+    )
+}
+export default InputTodo;
